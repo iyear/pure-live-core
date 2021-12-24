5079a1ee304f13cc332ada716559991da8f9ba00
iyear <ljyngup@gmail.com>
2021-12-24 13:59:30 +0800

    :sparkles: new version

--- global/version.go
+++ global/version.go
@@ -5,7 +5,9 @@ import (
 	"runtime"
 )
 
-const Version = "v0.1.0"
+const (
+	Version = "v0.1.0.211224-beta"
+)
 
 func GetRuntime() string {
 	return fmt.Sprintf("%s %s/%s", runtime.Version(), runtime.GOOS, runtime.GOARCH)
