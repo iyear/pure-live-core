b9eeb93884b1c1712fff22eac874d275f70f05a8
iyear <ljyngup@gmail.com>
2021-12-30 00:24:11 +0800

    :recycle: explicit ignore params

--- pkg/client/internal/abstract/base.go
+++ pkg/client/internal/abstract/base.go
@@ -6,18 +6,20 @@ import (
 	"github.com/iyear/pure-live/pkg/conf"
 )
 
-type Client struct {
-}
+type Client struct{}
 
 func (c *Client) Plat() string {
 	return conf.PlatAbstract
 }
 
 func (c *Client) GetPlayURL(room string, qn int) (*model.PlayURL, error) {
+	_ = room
+	_ = qn
 	return nil, errors.New("please override the GetPlayURL function")
 }
 
 func (c *Client) GetRoomInfo(room string) (*model.RoomInfo, error) {
+	_ = room
 	return nil, errors.New("please override the GetRoomInfo function")
 }
 
@@ -26,10 +28,13 @@ func (c *Client) Host() string {
 }
 
 func (c *Client) Enter(room string) (tp int, data [][]byte, err error) {
+	_ = room
 	return 0, nil, errors.New("please override the Enter function")
 }
 
 func (c *Client) Handle(tp int, data []byte) (msg []model.Msg, matched bool, err error) {
+	_ = tp
+	_ = data
 	return nil, false, errors.New("please override the Handle function")
 }
 
@@ -38,9 +43,11 @@ func (c *Client) HeartBeat() (tp int, data []byte, err error) {
 }
 
 func (c *Client) SendDanmaku(room string, content string, tp int, color int64) error {
+	_ = room
+	_ = content
+	_ = tp
+	_ = color
 	return errors.New("please override the SendDanmaku function")
 }
 
-func (c *Client) Stop() {
-
-}
+func (c *Client) Stop() {}
